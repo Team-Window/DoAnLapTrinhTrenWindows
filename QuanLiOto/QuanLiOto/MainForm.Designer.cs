f5106c20a67ad17e84c2648864c6b6065ac50676
= <=>
2020-04-07 20:13:32 +0700

    Button them trong QLGuiXe

---
+++ QuanLiOto/QuanLiOto/MainForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace QuanLiOto
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_TieuDe = new System.Windows.Forms.Label();
+            this.lbl_ThueXe = new System.Windows.Forms.Label();
+            this.lbl_GuiXe = new System.Windows.Forms.Label();
+            this.btn_GuiXe = new System.Windows.Forms.Button();
+            this.btn_ThueXeDap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_TieuDe
+            // 
+            this.lbl_TieuDe.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_TieuDe.Location = new System.Drawing.Point(0, -1);
+            this.lbl_TieuDe.Name = "lbl_TieuDe";
+            this.lbl_TieuDe.Size = new System.Drawing.Size(742, 98);
+            this.lbl_TieuDe.TabIndex = 0;
+            this.lbl_TieuDe.Text = "Phần mềm quản lý hệ thống chăm sóc và gởi xe khép kín";
+            this.lbl_TieuDe.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_ThueXe
+            // 
+            this.lbl_ThueXe.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ThueXe.Location = new System.Drawing.Point(291, 97);
+            this.lbl_ThueXe.Name = "lbl_ThueXe";
+            this.lbl_ThueXe.Size = new System.Drawing.Size(157, 77);
+            this.lbl_ThueXe.TabIndex = 1;
+            this.lbl_ThueXe.Text = "Thuê xe";
+            this.lbl_ThueXe.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_GuiXe
+            // 
+            this.lbl_GuiXe.AutoSize = true;
+            this.lbl_GuiXe.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_GuiXe.Location = new System.Drawing.Point(47, 115);
+            this.lbl_GuiXe.Name = "lbl_GuiXe";
+            this.lbl_GuiXe.Size = new System.Drawing.Size(117, 40);
+            this.lbl_GuiXe.TabIndex = 3;
+            this.lbl_GuiXe.Text = "Gửi xe";
+            // 
+            // btn_GuiXe
+            // 
+            this.btn_GuiXe.BackgroundImage = global::QuanLiOto.Properties.Resources.ImageGuiXe;
+            this.btn_GuiXe.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_GuiXe.Location = new System.Drawing.Point(63, 158);
+            this.btn_GuiXe.Name = "btn_GuiXe";
+            this.btn_GuiXe.Size = new System.Drawing.Size(87, 53);
+            this.btn_GuiXe.TabIndex = 4;
+            this.btn_GuiXe.UseVisualStyleBackColor = true;
+            this.btn_GuiXe.Click += new System.EventHandler(this.btn_GuiXe_Click);
+            // 
+            // btn_ThueXeDap
+            // 
+            this.btn_ThueXeDap.BackgroundImage = global::QuanLiOto.Properties.Resources.ImageXeDap;
+            this.btn_ThueXeDap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_ThueXeDap.Location = new System.Drawing.Point(312, 160);
+            this.btn_ThueXeDap.Name = "btn_ThueXeDap";
+            this.btn_ThueXeDap.Size = new System.Drawing.Size(64, 51);
+            this.btn_ThueXeDap.TabIndex = 2;
+            this.btn_ThueXeDap.UseVisualStyleBackColor = true;
+            this.btn_ThueXeDap.Click += new System.EventHandler(this.btn_ThueXeDap_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 360);
+            this.Controls.Add(this.btn_GuiXe);
+            this.Controls.Add(this.lbl_GuiXe);
+            this.Controls.Add(this.btn_ThueXeDap);
+            this.Controls.Add(this.lbl_ThueXe);
+            this.Controls.Add(this.lbl_TieuDe);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_TieuDe;
+        private System.Windows.Forms.Label lbl_ThueXe;
+        private System.Windows.Forms.Button btn_ThueXeDap;
+        private System.Windows.Forms.Label lbl_GuiXe;
+        private System.Windows.Forms.Button btn_GuiXe;
+    }
+}
+
---
+++ QuanLiOto/QuanLiOto/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLiOto
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ThueXeDap_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_GuiXe_Click(object sender, EventArgs e)
+        {
+            frmGuiXe guiXe=new frmGuiXe();
+            guiXe.ShowDialog(this);
+        }
+    }
+}
